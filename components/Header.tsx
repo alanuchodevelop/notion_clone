76448d4b03fe24ee5afaacbd039e2bd269ec4ced
alanuchodevelop <kerosen07@outlook.es>
2024-10-27 21:49:04 -0600

    Feature: first commit add clerk authentication (first steps)

---
+++ components/Header.tsx
@@ -0,0 +1,26 @@
+'use client'
+import React from 'react';
+import {SignedIn, SignedOut, SignInButton, UserButton, useUser} from "@clerk/nextjs";
+
+function Header() {
+    const { user } = useUser();
+    return (
+        <div>
+            {user && (
+                <h1>{user?.firstName}{`'s`} Space</h1>
+            )}
+            {/*breadcrumbs*/}
+            <div>
+                <SignedOut>
+                    <SignInButton/>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton/>
+                </SignedIn>
+            </div>
+        </div>
+    )
+
+}
+
+export default Header;
